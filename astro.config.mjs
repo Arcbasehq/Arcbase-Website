f3b6e0cb21dd54fdf405666dff10aec98fc0537c
Arcbasehq <grady.korchinski89@gmail.com>
2026-03-29 22:30:14 -0600

    fixed astro config for arcbase instead of criminas

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://criminas.org',
+    site: 'https://arcbase.one',
     integrations: [sitemap()],
 });
 
